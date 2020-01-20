43f7f59e620746f42d9e60cffad9e0318ff23020
Paul Augustine Cordova <royalcordova@protonmail.ch>
2020-01-19 21:51:18 -0500

    adjust xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,7 +6,7 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
 import System.IO
- 
+
 myManageHook = composeAll
     [ isFullscreen --> doFullFloat
     ]
@@ -18,11 +18,11 @@ main = do
                         <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
-        , modMask = mod4Mask     
-	, terminal = "st -e tmux"
-        } 
+        , modMask = mod4Mask
+	, terminal = "st -e tmux new -A -s main"
+        }
         `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "i3lock --color=000000")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
         , ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Iosevka-10'")
